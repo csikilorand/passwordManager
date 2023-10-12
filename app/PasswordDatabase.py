4dfaa4ddc7dc66d326d3c377878697e212ecf4d2
Csiki Lorand <csiki.lori95@gmail.com>
2023-10-12 22:23:14 +0300

    add PAsswordDatabase class with sqlite

---
+++ app/PasswordDatabase.py
@@ -0,0 +1,33 @@
+import sqlite3
+from errors import UnspecifiedDataError
+
+
+class PasswordDatabase():
+    def __init__(self, db_name, table_name):
+        self.db_name = db_name
+        self.table_name = table_name
+        self.connection = sqlite3.connect(self.db_name)
+        self.cursor = self.connection.cursor()
+        self.__create_table__()
+    
+           
+    def __create_table__(self):
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS passwords (
+                            service TEXT,
+                            username TEXT,
+                            password TEXT
+                        )''')
+        self.connection.commit()
+        
+    
+    def save_data(self, new_data:tuple):
+        website, username, password = new_data
+        self.cursor.execute(f"INSERT INTO {self.table_name} VALUES (?, ?, ?)", (website, username, password))
+        self.connection.commit()
+    
+    def retrieve_data(self, data):
+        pass
+            
+    def retrieve_data(self, what_to_retrieve:str):
+        pass
+            
