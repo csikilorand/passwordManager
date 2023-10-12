b081549de273c5bb4481352e848caa27295a3951
csikilorand <csiki.lori95@gmail.com>
2023-10-12 10:17:58 +0300

    added main.py and started error handling

---
+++ app/main.py
@@ -0,0 +1,31 @@
+from PasswordManager import PasswordManager
+import getpass
+
+myPasswordManager = PasswordManager('key.key', 'passwords.txt')
+myPasswordManager.save_password(service='website', username='google', password='12356')
+
+while True:
+    print("\nPassword Manager Menu:")
+    print("1. Save Password")
+    print("2. Get Password")
+    print("3. Quit")
+    choice = input("Enter your choice: ")
+    
+    if choice == '1':
+        service = input("Enter the service or website name: ")
+        username = input("Enter your username: ")
+        password = getpass.getpass("Enter your password: ")
+        myPasswordManager.save_password(service, username, password)
+        print("Password saved successfully!")
+
+    elif choice == '2':
+        service = input("Enter the service or website name: ")
+        username = input("Enter your username: ")
+        password = myPasswordManager.get_password(service, username)
+        if password:
+            print(f"Password: {password}")
+        else:
+            print("Password not found!")
+
+    elif choice == '3':
+        break
